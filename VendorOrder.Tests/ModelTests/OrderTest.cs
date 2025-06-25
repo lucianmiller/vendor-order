3b368d7c0c388e5531a63c88a16c6375096fc128
Lucian Miller <lucian.miller185@gmail.com>
2025-06-25 14:29:04 -0700

    Create Order Model

---
+++ VendorOrder.Tests/ModelTests/OrderTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrder.Models;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+  }
+}
