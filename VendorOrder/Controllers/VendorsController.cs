37242b528e09441d8b1a5d52488d18cdb0ab8248
Lucian Miller <lucian.miller185@gmail.com>
2025-07-11 14:38:17 -0700

    Add function to create orders for selected vendor

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -34,8 +34,21 @@ namespace VendorOrder.Controllers
       Vendor selectedVendor = Vendor.Find(id);
       List<Order> vendorOrders = selectedVendor.Order;
       model.Add("vendor", selectedVendor);
-      model.Add("orders", vendorOrders);
+      model.Add("order", vendorOrders);
       return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Order;
+      model.Add("order", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
     }
   }
 }
